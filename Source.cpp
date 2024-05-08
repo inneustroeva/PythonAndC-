aab6204bf5b9c4465e96a18bdceea6150be746c9
inneustroeva <147411094+inneustroeva@users.noreply.github.com>
2024-05-08 13:48:49 +0400

    С ++ Дихотомии

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,28 @@
-﻿
 #include <iostream>
-#include <complex>
-#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 using namespace std;
-int main() {
-	setlocale(LC_ALL, "Russian");
-	complex<double> z1(-1.0, 1.0);
-	complex<double> z2(-3.0, 2.0);
-	complex<double> z(-1.0, 2.0);
 
-	cout << "Сумма комплексных чисел: " << z1 + z2 << endl;
-	cout << "Разность комплексных чисел: " << z1 - z2 << endl;
-	cout << " Произведение комплексных чисел: " << z1 * z2 << endl;
-	cout << " Частное комплексных чисел: " << z1 / z2 << endl;
-	cout << " Возведение в степень комплексного чисела: " << pow(z, 4) << endl;
-	double rho = abs(z);    // модуль комплексного числа  
-	double theta = arg(z);  // аргумент комплексного числа
-
-	double r = pow(rho, 1.0 / 3.0);  // кубический корень из модуля
-	double t = theta / 3.0;    // деление аргумента на 3
-
-	complex<double> Root =polar(r, t); // Создание к.ч. с использованием модуля `r` и аргумента `t`
-
-	cout << "Корень третьей степени комплексного числа: " << Root <<endl;
-
+double f(double x) {
+	return exp(-x) - sqrt(x - 1);
+}
+double dixit(double a, double b, double eps) {
+	double c;
+	do {
+		c = (a + b) / 2.0;
+		if (f(a) * f(c) < 0) b = c;
+		else if (f(b) * f(c) < 0) a = c;
+		else {
+			cout << "The root is not found" << endl;
+			return 0;
+		}
+	} while (fabs(b - a) >= eps);
+	return c;
+}
+int main()
+{
+	double a = 0.9;
+	double b = 1.20;
+	double eps = 0.000001;
+	cout << dixit(a, b, eps);
 }
